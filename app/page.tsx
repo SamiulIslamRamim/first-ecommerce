221dd5f53d2d8303df333f32768bd78790510f09
SamiulIslamRamim <mohammadsamiulislamramim@gmail.com>
2026-04-05 16:51:14 +0600

    -Initialised Project -configured dependencies and other files

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='p-10'>
+      <h2 className='text-xl font-semibold'>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, iure nostrum dolorem aspernatur quia natus enim minima amet eos similique sapiente! Officiis, quos facere voluptate aliquid maxime laboriosam, libero omnis quae autem nam et saepe? Architecto quaerat voluptatibus minima! Repellendus itaque mollitia hic distinctio fugiat deserunt iusto possimus voluptatibus quia.</p>
+    </div>
+  )
+}
+
+export default Home
