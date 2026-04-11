a21eadce5e593256437d5187386ed3166fa79e92
Samiul Islam Ramim <123535324+SamiulIslamRamim@users.noreply.github.com>
2026-04-11 23:47:50 +0600

    -as before no updates....

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,4 +1,22 @@
+
 import React from 'react'
+import { YoutubeIcon } from './icons/lucide-youtube'
+const socialLink = [
+  {title: "Youtube",
+  href: "https://www.youtube.com/",
+  icon: <YoutubeIcon />},
+
+  // title: "Facebook",
+  // href: "https://www.facebook.com/"
+  // icon:
+  // title:
+  // href:
+  // icon:
+  // title:
+  // href:
+  // icon:
+]
+
 
 const SocialMedia = () => {
   return (
