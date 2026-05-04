31e1d39220590da084c45debca5b5403e65c4c75
SamiulIslamRamim <mohammadsamiulislamramim@gmail.com>
2026-05-04 23:51:15 +0600

    'formatting'

--- sanity/queries/query.ts
+++ sanity/queries/query.ts
@@ -1,23 +1,20 @@
-import { log } from "console"
+import { log } from "console";
 import { sanityFetch } from "../lib/live";
 
-const getCategories= async(quantity?: number)=>{
-    try {
-        const query = quantity
-        ? `*[_type == 'category] | order(name asc) [0...$quantity]{..., "productCount":count(*[_type == "product" && references(^.id)])}`
-        :
-        `*[_type == 'category] | order(name asc) {..., "productCount":count(*[_type == "product" && references(^.id)])}`;
-        const { data } = await sanityFetch({
-            query,
-            params: quantity ? {quantity} : {},
-        });
-        return data;
-    } catch (error) {
-        console.log("Error fetching categories", error);
-        return [];
-    }
-}
+const getCategories = async (quantity?: number) => {
+  try {
+    const query = quantity
+      ? `*[_type == 'category] | order(name asc) [0...$quantity]{..., "productCount":count(*[_type == "product" && references(^.id)])}`
+      : `*[_type == 'category] | order(name asc) {..., "productCount":count(*[_type == "product" && references(^.id)])}`;
+    const { data } = await sanityFetch({
+      query,
+      params: quantity ? { quantity } : {},
+    });
+    return data;
+  } catch (error) {
+    console.log("Error fetching categories", error);
+    return [];
+  }
+};
 
-
-
-export {getCategories};
+export { getCategories };
