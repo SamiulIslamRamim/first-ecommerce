56d69e2ad22084647ebaae00d45cfd9254d873ba
SamiulIslamRamim <mohammadsamiulislamramim@gmail.com>
2026-06-08 15:44:57 +0600

    -homceproduct fetch fix

--- sanity/queries/index.ts
+++ sanity/queries/index.ts
@@ -4,8 +4,14 @@ import { BRAND_QUERY, BRANDS_QUERY, DEAL_PRODUCTS, LATEST_BLOG_QUERY, PRODUCT_BY
 const getCategories = async (quantity?: number) => {
   try {
     const query = quantity
-      ? `*[_type == 'category'] | order(name asc) [0...$quantity]{..., "productCount":count(*[_type == "product" && references(^._id)])}`
-      : `*[_type == 'category'] | order(name asc) {..., "productCount":count(*[_type == "product" && references(^._id)])}`;
+      ? `*[_type == 'category'] | order(name asc) [0...$quantity] {
+          ...,
+          "productCount": count(*[_type == "product" && references(^._id)])
+        }`
+      : `*[_type == 'category'] | order(name asc) {
+          ...,
+          "productCount": count(*[_type == "product" && references(^._id)])
+        }`;
     const { data } = await sanityFetch({
       query,
       params: quantity ? { quantity } : {},
